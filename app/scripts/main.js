856cf7bda6baaaa42386fcf9fe5d22c4e1287f1d
Chris Given <cmgiven@gmail.com>
2015-08-29 11:20:42 -0400

    Initial web commit

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+/*jslint browser: true*/
+/*jslint nomen: true*/
+/*global $, _, d3*/
+
+(function () {
+    'use strict';
+
+    var app;
+
+    $(function () {
+    });
+
+    app = {
+        initialize: function (data) {
+        },
+    };
+
+    window.app = app;
+
+}());
